a35de318a9b771ec5a14776b07fd420680872d7d
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2023-11-06 15:53:25 -0500

    feat: add github updates

--- private/class-main.php
+++ private/class-main.php
@@ -7,6 +7,7 @@ require_once __DIR__.'/custom-post-type/class-utils.php';
 require_once __DIR__.'/custom-post-type/class-device.php';
 require_once __DIR__.'/custom-post-type/class-sms.php';
 require_once __DIR__.'/class-sms-gateway-press.php';
+require_once __DIR__.'/class-gh-auto-updater.php';
 
 abstract class Main
 {
@@ -18,5 +19,17 @@ abstract class Main
 		Custom_Post_Type\Sms::register();
 
 		Rest_Api::register_endpoints();
+
+		add_action( 'init', array( __CLASS__, 'activate_autoupdate' ) );
+	}
+
+	public static function activate_autoupdate(): void
+	{
+		$plugin_slug = plugin_basename( SMS_GATEWAY_PRESS_DIR.'/plugin.php' );
+		$gh_user = 'SMS-Gateway-Press';
+		$gh_repo = 'wp-plugin';
+
+		// Activate automatic update.
+		new GH_Auto_Updater( $plugin_slug, $gh_user, $gh_repo );
 	}
 }
